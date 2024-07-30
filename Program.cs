02990d4314fc4a451876a6d3bec00b6308598a57
Michael Oyelami <michael.oyelami@frontdoor.com>
2024-07-30 18:37:00 +0100

    first azure app service commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseDeveloperExceptionPage();
+app.UseSwagger();
+app.UseSwaggerUI(x =>
+{
+
+    x.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API V1");
+
+#if DEBUG
+    x.RoutePrefix = "swagger"; // For localhost
+#else
+    x.RoutePrefix = string.Empty; //  For azure
+#endif
+}
+);
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
